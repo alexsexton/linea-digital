8f67bcda01fb3cb0909f89d6efd2c938f580c2f3
Alex Sexton <alex@threefour.co.uk>
2020-05-16 17:01:39 +0200

    adjust padding on sections

--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -1,83 +1,24 @@
-/* globals jQuery, Modernizr */
+/* globals */
 'use strict'
 
-jQuery(function ($) {
-  // ## Navigation
-  var $navHandler = $('.site-nav--trigger')
-  var $subnavHandler = $('.nav-subnav--handler')
+const navItems = document.querySelectorAll('.main-nav a')
 
-  var bodyOpenClasses = [
-    'subnav--open',
-    'site-nav--open'
-  ]
+for (let item = 0; item < navItems.length; item++) {
+  const itemHash = navItems[item].hash
 
-  // ## Mobile nav trigger
-  $navHandler.on('click', function (e) {
-    e.preventDefault()
-    var navBodyOpenClass = 'site-nav--open'
-    $('body').toggleClass(navBodyOpenClass)
-    $('*').removeClass('is-open')
-  })
+  console.log(itemHash)
+  // item.addEventListener('click', function (e) {
+  //   itemHash.scrollIntoView({ behavior: 'smooth' }).scrollBy({ top: -100 })
+  // })
+}
 
-  // ## Sub Nav
-  $subnavHandler.each(function (i) {
-    var $this = $(this)
-    var $subnav = $this.next()
-    var subNavOpenClass = 'c-subnav--open'
-    var subNavBodyOpenClass = 'subnav--open'
-
-    $navHandler.on('click', function (e) {
-      e.preventDefault()
-      var isOpen = $subnav.hasClass(subNavOpenClass)
-      if (isOpen) {
-        $subnav.removeClass(subNavOpenClass)
-      }
-      var hasClass = $('body').hasClass(subNavBodyOpenClass)
-      if (hasClass) {
-        $('body').removeClass(subNavBodyOpenClass)
-      }
-    })
-
-    $this.on('click', function (e) {
-      e.preventDefault()
-      $('body').removeClass(bodyOpenClasses)
-      var isOpen = $subnav.hasClass(subNavOpenClass)
-      // remove all open subnav classes, therefore closing any that are open
-      $('.' + subNavOpenClass).removeClass(subNavOpenClass)
-      // if the corresponding subnav was closed already, user intends to open it
-      if (!isOpen) {
-        $subnav.addClass(subNavOpenClass)
-        $('body').addClass(subNavBodyOpenClass)
-      }
-    })
-  }).bind()
-
-  // ## Swap image on hover
-  var productImageHover = function () {
-    if (!Modernizr.touchevents) {
-      $(document).on('mouseenter mouseleave', '.c-image-thumbnail--hover img', function () {
-        var swapImg = this.getAttribute('data-swap-src')
-        var img = this.getAttribute('src')
-        $(this).attr('src', swapImg)
-        $(this).attr('data-swap-src', img)
-      }, function () {
-        var swapImg = this.getAttribute('data-swap-src')
-        var img = this.getAttribute('src')
-        $(this).attr('src', swapImg)
-        $(this).attr('data-swap-src', img)
-      })
-    }
-  }
-
-  // Call on resize
-  $(window).resize(function () {
-    productImageHover()
-  })
-  // Call functions on ready
-  productImageHover()
-})
-
-// ## Add loaded class once everything has loaded
-jQuery(document).ready(function ($) {
-  $('html').addClass('loaded')
-})
+// function nav () {
+//   const servicesNav = document.querySelector('.nav-services a')
+//   const servicesElm = document.querySelector('#services')
+//   servicesNav.addEventListener('click', function (event) {
+//     event.preventDefault()
+//     servicesElm.scrollIntoView({ behavior: 'smooth' }).scrollBy({ top: -100 })
+//   })
+// }
+//
+// nav()
