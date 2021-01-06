e2897b80986de3f4eda91bcab3f1b32875268fe8
Alex Sexton <alex@threefour.co.uk>
2021-01-06 16:25:18 +0000

    Add: scroll class with js magic and update colour scheme

--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -1,4 +1,38 @@
 /* globals */
 'use strict'
 
-// const navItems = document.querySelectorAll('.main-nav a')
+const body = document.body
+const scrollUp = 'scroll-up'
+const scrollDown = 'scroll-down'
+const scrollTop = 'scroll-top'
+const scrollBottom = 'scroll-bottom'
+let lastScroll = 0
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset
+  if (currentScroll <= 0) {
+    // bottom
+    body.classList.remove(scrollUp)
+    body.classList.add(scrollTop)
+    return
+  }
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollTop)
+    body.classList.remove(scrollUp)
+    body.classList.add(scrollDown)
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollTop)
+    body.classList.remove(scrollDown)
+    body.classList.add(scrollUp)
+  }
+  lastScroll = currentScroll
+
+  // bottom
+  if ((window.innerHeight + currentScroll) >= document.body.offsetHeight) {
+    body.classList.add(scrollBottom)
+  } else {
+    body.classList.remove(scrollBottom)
+  }
+})
